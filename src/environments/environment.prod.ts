798d56e4a3695b50ecf01bd091a5005affc2d74a
muenalakevin <kmmuenala@espe.edu.ec>
2022-02-04 00:25:26 -0500

    aws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'http://192.168.1.8:3000/api',
-  SOCKET_URL: 'http://192.168.1.8:3000/api'
+  API_URL: 'http://3.129.61.154:3000/api',
+  SOCKET_URL: 'http://3.129.61.154:3000/api'
 };
